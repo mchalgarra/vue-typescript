aae16a37299f1bd128fc62fa6a3fe52d10186ce2
Michell Algarra <michell.algarra@gmail.com>
2021-08-10 19:08:36 -0300

    feat(views/Auth): created the auth page

---
+++ src/views/Auth.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Auth</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { IUserData } from "@/models/interfaces/user";
+
+export default Vue.extend({
+  name: "Auth",
+  props: {
+    isSignUp: Boolean,
+  },
+  computed: {
+    user(): IUserData | null {
+      return this.$store.getters.user;
+    },
+  },
+  beforeMount() {
+    if (this.user) {
+      this.$router.push("/");
+    }
+    console.log(this.isSignUp);
+  },
+});
+</script>
