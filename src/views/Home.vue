66a83eeb15e7bed62f8ecb1a6a6e6647bbe3c47a
Michell Algarra <michell.algarra@gmail.com>
2021-08-10 19:13:07 -0300

    feat(views/Home): started to implement the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,45 @@
 <template>
-  <hello-world />
+  <v-container class="pa-0 ma-0" fluid>
+    <v-toolbar color="deep-purple" dark>
+      <v-row class="pa-0 ma-0" justify="center" align="center">
+        <v-col cols="2"></v-col>
+        <v-col cols="8" style="text-align: center">
+          <v-toolbar-title style="font-size: 1.4rem">
+            Vue TypeScript
+          </v-toolbar-title>
+        </v-col>
+        <v-col cols="2" style="text-align: right">
+          <v-btn text @click="!!user ? logout() : login()">{{
+            !!user ? "Logout" : "Login"
+          }}</v-btn>
+        </v-col>
+      </v-row>
+    </v-toolbar>
+  </v-container>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import { IUserData } from "@/models/interfaces/user";
 
-export default Vue.extend({
+export default {
   name: "Home",
-
-  components: {
-    HelloWorld,
+  components: {},
+  computed: {
+    isDark(): boolean {
+      return this.$store.getters.isDark;
+    },
+    user(): IUserData | null {
+      return this.$store.getters.user;
+    },
   },
-});
+  methods: {
+    login(): void {
+      console.log("Login");
+      this.$router.push("login");
+    },
+    logout(): void {
+      console.log("Logout");
+    },
+  },
+};
 </script>
