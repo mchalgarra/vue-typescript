b71afcb63699e5e8a32daea9d1dc996d7d582cef
Michell Algarra <michell.algarra@gmail.com>
2021-08-10 19:13:42 -0300

    feat(router): removed the about page and added the auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,38 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
+import Auth from '../views/Auth.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/login',
+    name: 'Login',
+    // component: () => import('../views/Auth.vue'),
+    component: Auth,
+    props: {
+      isSignUp: false,
+    },
+  },
+  {
+    path: '/sign-up',
+    name: 'Sign Up',
+    // component: () => import('../views/Auth.vue'),
+    component: Auth,
+    props: {
+      isSignUp: true,
+    },
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
 });
