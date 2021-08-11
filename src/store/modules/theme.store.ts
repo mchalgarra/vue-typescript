c8f478516c385ffec591b54e53cd14d185e1a24a
Michell Algarra <michell.algarra@gmail.com>
2021-08-11 19:03:49 -0300

    docs(store/theme): documented the Theme store module

--- src/store/modules/theme.store.ts
+++ src/store/modules/theme.store.ts
@@ -1,5 +1,10 @@
 import { IThemeGetters, IThemeState } from '@/models/interfaces/theme';
 
+/**
+ * Instantiates the inicial Theme module state.
+ *
+ * @returns The Theme module state.
+ */
 const initialState = (): IThemeState => ({
   theme: 'light',
 });
@@ -11,12 +16,30 @@ const mutations = {};
 const actions = {};
 
 const getters: IThemeGetters = {
+  /**
+   * Gets the current theme.
+   *
+   * @param state - The Theme module state.
+   * @returns The current theme.
+   */
   theme: (state: IThemeState): string => {
     return state.theme;
   },
+  /**
+   * Gets whether the current theme is light or not.
+   *
+   * @param state - The Theme module state.
+   * @returns Whether the current theme is light or not.
+   */
   isLight: (state: IThemeState): boolean => {
     return state.theme === 'light';
   },
+  /**
+   * Gets whether the current theme is dark or not.
+   *
+   * @param state - The Theme module state.
+   * @returns Whether the current theme is dark or not.
+   */
   isDark: (state: IThemeState): boolean => {
     return state.theme === 'dark';
   },
